29b9b22169c54342cc7658c6a50eeb7b5502b15e
Timp Yorke <thongphitak.tim@gmail.com>
2024-06-30 11:17:41 +0700

    fix route

--- index.js
+++ index.js
@@ -58,57 +58,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('api/v1/thailand', (_, res) => {
-
-    // Set province, district, and sub-districts
-    const provinces = provinceData.map((province) => new Province(province));
-    const districts = districData.map((district) => new District(district));
-    const subDistricts = subDistriceData.map((subDistrict) => new SubDistrict(subDistrict));
-
-    // Map subDistricts to their respective districts
-    const subDistrictsByDistrictId = subDistricts.reduce((acc, subDistrict) => {
-        const districtId = subDistrict.district_id;
-        if (!acc[districtId]) {
-            acc[districtId] = [];
-        }
-        acc[districtId].push(subDistrict);
-        return acc;
-    }, {});
-
-
-    // Update districts with subDistricts
-    districts.forEach((district) => {
-        const districtId = district.id;
-        const subDistrictsForDistrict = subDistrictsByDistrictId[districtId];
-        if (subDistrictsForDistrict) {
-            subDistrictsForDistrict.forEach((subDistrict) => {
-                district.addSubDistrict(subDistrict);
-            });
-        }
-    });
-
-    // Update provinces with districts
-    provinces.forEach((province) => {
-        districts.forEach((district) => {
-            if (district.province_id === province.id) {
-                province.addDistrict(district);
-            }
-        });
-    });
-
-    // Reander
-    res.send(provinces)
-})
-
-app.get('api/v1/thailand/provinces', (_, res) => {
+app.get('/api/v1/thailand/provinces', (_, res) => {
     res.send(provinceData)
 })
 
-app.get('/v1/thailand/districts', (_, res) => {
+app.get('/api/v1/thailand/districts', (_, res) => {
     res.send(districData)
 })
 
-app.get('/v1/thailand/subDistricts', (_, res) => {
+app.get('/api/v1/thailand/subDistricts', (_, res) => {
     res.send(subDistriceData)
 })
 
